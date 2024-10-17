7717538e3147bf251ce4a3b5dac8398917d94c4b
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-10-17 15:31:46 -0300

    Create index.js

---
+++ Aula_16_10_2024/index.js
@@ -0,0 +1,24 @@
+const express = require('express') //instanciando o módulo (express)
+const app = express()
+const port = 3000
+
+//rotas
+app.get('/', (req, res) => {
+  res.send('Página Inicial!')
+})
+
+app.get('/sobre', (req, res) => {
+    res.send('Página Sobre');
+})
+
+//middleware para lidar com as rotas do tipo post
+app.use(express.urlencoded({extended: true}));
+
+app.post('/adicionar', (req, res) =>{  //localhost:3000/adicionar/nome=Ana (postman ou Curl)
+    const nome = req.body.nome;
+    res.send(`Nome: ${nome}`);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
